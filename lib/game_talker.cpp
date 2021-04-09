656272572a6925675f5ba365449c7daa75534d3a
Justin Chen <justinbrianchen@yahoo.com>
2021-04-09 00:46:20 -0400

    add history functionality

--- lib/game_talker.cpp
+++ lib/game_talker.cpp
@@ -14,6 +14,8 @@ std::vector<std::string> tba::DefaultGameTalker::getInput()
         std::getline(std::cin, input);
     }
 
+    history.emplace_back(input);
+
     // tokenize input by whitespace
     std::istringstream iss(input);
     std::vector<std::string> args{std::istream_iterator<std::string>{iss},
